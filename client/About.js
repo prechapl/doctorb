598d1ad8fe9acf14f4b60cd728535301a859985f
prechapl <45429403+prechapl@users.noreply.github.com>
2023-08-04 20:49:46 -0400

    updated new cv

--- client/About.js
+++ client/About.js
@@ -24,7 +24,7 @@ class About extends Component {
               />
 
               <Button
-                href="/cv.pdf"
+                href="/cv_7-23.pdf"
                 className="mt-4"
                 style={{
                   background: "#74b4ca",
